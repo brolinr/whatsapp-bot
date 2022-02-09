f064a8eb6d016360b62b8856eef6afb9a758dcd8
brolinr <brolinremz@gmail.com>
2022-02-09 18:55:17 +0200

    Adding more functionality

--- bot.rb
+++ bot.rb
@@ -5,50 +5,52 @@ class WhatsAppBot < Sinatra::Base
   use Rack::TwilioWebhookAuthentication, ENV['TWILIO_AUTH_TOKEN'], '/bot'
 
   get '/' do
-    "Hello World!"
+    "hello world"
   end
 
   post '/bot' do
     body = params["Body"].downcase
+    phone = params["from"]
+    name = params["name"]
     response = Twilio::TwiML::MessagingResponse.new
     #name = User.username #store the sender's username
     #phone = User.phone #store the sender's phone
 
     response.message do |message|
-      greeting = ["hie", "hi", "ndeip", "hey", "hello"]
-      greeting.each do |g|
-        if body.include?(g)
-          message.body("Welcome Customer to our bot by pressing 1 or sending 
-                        'agree' you agree to our terms and conditions outline below :")
-        end
+      
+      if body.include?("hie") || body.include?("hi") || body.include?("ndeip") || 
+         body.include?("hello") || body.include?("hey")
+        message.body("Welcome Customer to our bot by sending 1 or 'agree' you agree to our terms and conditions outline below.")
       end
 
       if body.include?("agree") || body.include?("1")
         Customer.register(name, phone)
         message.body("Congratulations Customer 
-                      Your account has been successifully created: \n
-                      1. Type 'Available houses' to view all the houses available.\n
+                      Your account has been successifully created: \n\n
+                      1. Type 'Available houses' to view all the houses available.\n\n
                       2. Type 'Subscribe' to pay a monthly suscription fee of $10")
       end
 
       if body.include?("available houses")
-        message.body(Property.index)
-        sleep 2
+        Property.index.each do |property|
+          message.body("City:\t\t#{property["city"].to_s}\n\n
+                      Description:\t\t#{property["description"].to_s}\n\n")
+        end
         message.body("Customer, Enter the '@' symbol along with the 
                     number assigned to the house that interests you, for
-                    example: \n Type @1 to view the house assigned to 1`")
+                    example: \n\n Type @1 to view the house assigned to 1`")
       end
       
       if body.include?("subscribe")
-        message.body("Please type in the ecocash number which you will use to pay for the subscription,
-                      For example 0787777777")
+        message.body("Please type in the ecocash number which you will use to pay for the subscription:\n
+                      `For example 0787777777`")
       end
       
       if body.include?("078") || body.include?("077")
         Customer.subscribe(phone, body)
         message.body("Thank you Customer for paying you monthly subscription
-                      to use our service. Please: \n
-                      1. Type 'search' to search any available property.\n
+                      to use our service. Please: \n\n
+                      1. Type 'search' to search any available property.\n\n
                       2. Type 'Available houses' to view the list of all the houses available.")
       end
 
@@ -71,3 +73,4 @@ class WhatsAppBot < Sinatra::Base
     response.to_xml
   end
 end
+
--- bot_logic.rb
+++ bot_logic.rb
@@ -3,12 +3,15 @@ require "json"
 require "net/http"
 
 module Property
-    url = "https://api-bluffhope.herokuapp.com/properties/"
-
     def self.index
-        response = HTTP.get(url)
-        #get the list of available houses and format them and store them in a hash
-        JSON.parse(response.to_s)["properties"].first
+      url = URI("https://api-bluffhope.herokuapp.com/properties/")
+
+      http = Net::HTTP.new(url.host, url.port);
+      request = Net::HTTP::Get.new(url)
+      
+      response = http.request(request)
+       g=response.read_body
+       JSON.parse(g)
     end
   
     def self.show(phone, id)
@@ -55,17 +58,48 @@ module Property
       request = Net::HTTP::Post.new(url)
       request["Content-Type"] = "application/json"
       request.body = JSON.dump({
-        "name": name,
+        "city": name,
         "address": address,
         "description": description,
         "contact": contact,
         "user_id": "1"
       })
+
       response = http.request(request)
-      product = response.read_body
+      house = response.read_body
+      house = JSON.parse(house)
+      
+      house.each do |product|
+        message.body("
+          City:\t\t#{house["city"].to_s}\n\n
+          Address:\t\t#{house["address"].to_s}\n\n
+          Contact:\t\t#{house["contact"].to_s}\n\n
+          You have successifully added a house listing!")
+      end
     end
 
     def self.update_property
       JSON.parse(response.to_s)["message"]
     end
+
+    def self.set_amount(price)
+      url = URI("https://api-bluffhope.herokuapp.com/amounts")
+
+      http = Net::HTTP.new(url.host, url.port);
+      request = Net::HTTP::Post.new(url)
+      request["Content-Type"] = "application/json"
+      request.body = JSON.dump({
+        "price": price,
+        "user_id": "1"
+      })
+
+      response = http.request(request)
+      response = JSON.parse(response)
+      
+      response.each do |price|
+        message.body("You have successifully set the subscription
+                      price to #{price["price"]}")
+        end
+    end
+    
   end
